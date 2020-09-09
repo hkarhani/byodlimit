acf8c8b64175813676ab7cfcf0c48088fd1288ce
Hassan El Karhani <hkarhani@gmail.com>
2020-09-09 14:03:28 +0400

    adding api key authentication

--- README.md
+++ README.md
@@ -1,32 +1,38 @@
 # Enforcing BYOD devices Limit per User
 
-fastAPI Service to help in limiting BYOD devices count per User on Forescout Platform. 
+fastAPI Service to help in limiting BYOD devices count per User on Forescout Platform.
 
 ## 1. Preparing the Docker image
 
-Either by building it locally on your Docker host: 
+Either by building it locally on your Docker host:
 `git clone https://github.com/hkarhani/byodlimit.git`
 
-and then build it within the downloaded folder 'byodlimit': 
+and then build it within the downloaded folder 'byodlimit':
 `docker build -t byodlimit .`
 
-or simply pull the auto-built image: 
+or simply pull the auto-built image:
 `docker pull hkarhani/byodlimit`
 
 ## 2. Run the Docker image
 
-This image will have both Jupyter Notebook (exposed port 8888) and fastAPI (exposed port 5000) running. 
+This image will have both Jupyter Notebook (exposed port 8888) and fastAPI (exposed port 5000) running.
 
-If you pulled the auto-built image you can run the container by executing the following command: 
-`docker run -itd -p 8888:8888 -p 5000:5000 hkarhani/byodlimit` 
+If you pulled the auto-built image you can run the container by executing the following command:
+`docker run -itd -p 8888:8888 -p 5000:5000 hkarhani/byodlimit`
 
-otherwise, you can run your custom built image: 
+otherwise, you can run your custom built image:
 
-`docker run -itd -p 8888:8888 -p 5000:5000 byodlimit` 
+`docker run -itd -p 8888:8888 -p 5000:5000 byodlimit`
 
 ## 3. Edit the fsconfig.yml file
 
-In order for pyFS library to properly POST DEX attributes to Forescout platform, please ensure you edit the fsconfig.yml with the IP and credentials of your Forescout Platform running OIM. Provide both web-api (not used in this code) / DEX (used to update the guest_tag DEX attribute). Please note that username for DEX needs to be in format: name@username as per CounterACT Web Service Accounts configuration. 
+In order for pyFS library to properly POST DEX attributes to Forescout platform, please ensure you edit the fsconfig.yml with the IP and credentials of your Forescout Platform running OIM. Provide both web-api (not used in this code) / DEX (used to update the guest_tag DEX attribute). Please note that username for DEX needs to be in format: name@username as per CounterACT Web Service Accounts configuration.
+
+Please edit main.py to change:
+- maxDevicesPerUser = 2 # default it is 2 concurrent BYOD devices.
+- API_KEY = "1234567asdfgh" # Please change the API Key
+
+You can change other API_KEY_NAME or COOKIE_DOMAIN parameters while reflecting those changes in your DEX POST Web Service Request.
 
 ## 4. Run the fastAPI web-service on the container
 
@@ -34,10 +40,18 @@ Use the following command to run the fastAPI service. You can either execute it 
 
 `uvicorn main:app --host 0.0.0.0 --port 5000 --reload`
 
-You will be able to monitor the fastAPI service from the command output. 
+You will be able to monitor the fastAPI service from the command output.
 
-## P.S. Security Note: 
+## P.S. Security Note:
 
-Please use this script in controlled Lab Environment - Security features are disabled in the provided script. 
+Please use this script in controlled Environment.
 
-Security features are disabled for Jupyter Notebook (anyone with IP reachability to docker host will be able to browse the Notebook) and fastAPI is implementing http only without authentication. Will be updating api-key verification or credentials-based authentication in future release. 
+Security features are disabled for Jupyter Notebook (anyone with IP reachability to docker host port 8888 will be able to browse the Notebook). You can either modify the Jupyter Notebook settings file or stop sharing the port 8888. 
+
+
+fastAPI (default port 5000) is implementing http only and I just updated the code the accept API Key either via: 
+- URL Query: /apipost/?access_token=api-key
+- or Authentication Headers - by adding header: access_token:api-key 
+- or Cookie (COOKIE_DOMAIN: access_token:api-key)
+
+
--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-from fastapi import FastAPI
+from fastapi import Security, Depends, FastAPI, HTTPException
+from fastapi.security.api_key import APIKeyQuery, APIKeyCookie, APIKeyHeader, APIKey
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.openapi.utils import get_openapi
+
+from starlette.status import HTTP_403_FORBIDDEN
+from starlette.responses import RedirectResponse, JSONResponse
+
 from pydantic import BaseModel
+import uvicorn
 
 from pyFS import pyFS
 
@@ -8,8 +16,40 @@ import warnings
 warnings.simplefilter('ignore')
 logging.basicConfig(level=logging.INFO)
 
+# Max Devices Per User - can be updated.     
+maxDevicesPerUser = 2
+
+# Main Users Object to track logged-in Devices per user
+Users = {}
+
+# API Key Settings
+API_KEY = "1234567asdfgh" # Please Change the API Key 
+API_KEY_NAME = "access_token"
+COOKIE_DOMAIN = "byodlimit.fs"
+
+api_key_query = APIKeyQuery(name=API_KEY_NAME, auto_error=False)
+api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
+api_key_cookie = APIKeyCookie(name=API_KEY_NAME, auto_error=False)
+
+async def get_api_key(
+    api_key_query: str = Security(api_key_query),
+    api_key_header: str = Security(api_key_header),
+    api_key_cookie: str = Security(api_key_cookie),
+):
+
+    if api_key_query == API_KEY:
+        return api_key_query
+    elif api_key_header == API_KEY:
+        return api_key_header
+    elif api_key_cookie == API_KEY:
+        return api_key_cookie
+    else:
+        raise HTTPException(
+            status_code=HTTP_403_FORBIDDEN, detail="Could not validate credentials"
+        )
+        
 # app: Main FastAPI Object to be used
-app = FastAPI()
+app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
 
 # fsApp: pyFS Object to POST DEX Property to Forescout Platform 
 # by default Object will load fsconfig.yml file as input. 
@@ -24,11 +64,6 @@ class logEvent(BaseModel):
     mac: str
     authLogin: str
 
-# Max Devices Per User - can be updated.     
-maxDevicesPerUser = 2
-
-# Main Users Object to track logged-in Devices per user
-Users = {}
 
 # Function to add new Device to User / newUser 
 def addUserDev(Users, newUser, device, maxDevicesPerUser = 2):
@@ -63,18 +98,49 @@ def delUserDev(Users, existingUser, device):
 
 @app.get("/")
 async def read_root():
-    return {"message": "Welcome to Devices per User Count Enforcement using fastAPI!"}
+    return {"message": "Welcome to Devices per User Count Enforcement."}
+
+@app.get("/logout")
+async def route_logout_and_remove_cookie():
+    response = RedirectResponse(url="/")
+    response.delete_cookie(API_KEY_NAME, domain=COOKIE_DOMAIN)
+    return response
+
+@app.get("/openapi.json", tags=["documentation"])
+async def get_open_api_endpoint(api_key: APIKey = Depends(get_api_key)):
+    response = JSONResponse(
+        get_openapi(title="FastAPI security test", version=1, routes=app.routes)
+    )
+    return response
+
+@app.get("/documentation", tags=["documentation"])
+async def get_documentation(api_key: APIKey = Depends(get_api_key)):
+    response = get_swagger_ui_html(openapi_url="/openapi.json", title="docs")
+    response.set_cookie(
+        API_KEY_NAME,
+        value=api_key,
+        domain=COOKIE_DOMAIN,
+        httponly=True,
+        max_age=1800,
+        expires=1800,
+    )
+    return response
+
+@app.get("/test")
+async def get_open_api_endpoint(api_key: APIKey = Depends(get_api_key)):
+    response = "Authentication Test is successful!"
+    return response
 
 @app.get("/users")
-async def get_users():
+async def get_users(api_key: APIKey = Depends(get_api_key)):
     return Users
 
 @app.get("/maxdevices")
-async def max_devices():
+async def max_devices(api_key: APIKey = Depends(get_api_key)):
     return {"maxDevicesPerUser":maxDevicesPerUser} 
 
 @app.post("/apipost/")
-async def create_logEvent(_logevent: logEvent):
+async def create_logEvent(_logevent: logEvent, api_key: APIKey = Depends(get_api_key)):
     logDict = _logevent.dict()
     if logDict['action'] == 'Login':       
         ret = addUserDev(Users, logDict['http_login_user'], logDict['access_ip'])
@@ -92,3 +158,6 @@ async def create_logEvent(_logevent: logEvent):
     else: 
         logging.info(f"action is not recognized! Log event: {logDict}")
     return {"status":"ok"}
+                         
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=5000)
